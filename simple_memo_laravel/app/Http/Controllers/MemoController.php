7f77d66f29461516d31c26a570103240a90723fc
Airi-reformas <airi_inoue@reformas.co.jp>
2023-07-14 10:37:03 +0900

    first commit

---
+++ simple_memo_laravel/app/Http/Controllers/MemoController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Memo;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class MemoController extends Controller
+{
+    public function index()
+    {
+        $memos = Memo::where('user_id', Auth::id())->orderBy('updated_at', 'desc')->get();
+        // dd($memos = Memo::where('user_id', Auth::id())->orderBy('updated_at', 'desc')->toArray());
+        // $memos = Memo::where('user_id', Auth::id())->orderBy('updated_at')->get();
+
+    return view('memo',[
+        'name' => $this->getLoginUserName(),
+        'memos' => $memos,
+        'select_memo'=>session()->get('select_memo')
+    ]);
+        
+ 
+    }
+
+    public function add(){
+        Memo::create([
+            'user_id'=> Auth::id(),
+            'title'=>'新規メモ',
+            'content'=>"",
+        ]);
+
+        return redirect()->route('memo.index');
+    }
+
+    public function update(Request $request)
+{
+    $memo = Memo::find($request->edit_id);
+    $memo->title = $request->edit_title;
+    $memo->content = $request->edit_content;
+
+    if ($memo->update()) {
+        session()->put('select_memo', $memo);
+    } else {
+        session()->remove('select_memo');
+    }
+
+    return redirect()->route('memo.index');
+}
+
+    private function getLoginUserName() {
+        $user = Auth::user();
+
+        $name = '';
+        if ($user) {
+            if (7 < mb_strlen($user->name)) {
+                $name = mb_substr($user->name, 0, 7) . "...";
+            } else {
+                $name = $user->name;
+            }
+        }
+
+        return $name;
+
+    }
+    
+    public function select(Request $request){
+        $memo = Memo::find($request->id);
+        session()->put('select_memo',$memo);
+
+        return redirect()->route('memo.index');
+
+    }
+
+    public function delete(Request $request)
+{
+    Memo::find($request->edit_id)->delete();
+    session()->remove('select_memo');
+
+    return redirect()->route('memo.index');
+}
+
+
+}
